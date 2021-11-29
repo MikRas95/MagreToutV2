d08df338760d7856209b19321411849164507c0d
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2021-11-29 10:43:36 +0100

    Employee create method - Nico

--- MagreToutV2/Services/EFServices/EmployeeService.cs
+++ MagreToutV2/Services/EFServices/EmployeeService.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MalgreToutV2.Models;
+using MalgreToutV2.Services.Interface;
 
 namespace MalgreToutV2.Services.EFServices
 {
-    public class EmployeeService
+    public class EmployeeService : IEmployee
     {
+        
+        private MalgretoutDBContext context;
+
+        public EmployeeService(MalgretoutDBContext Context)
+        {
+            context = Context;
+            
+        }
+        public void AddEmployee(DemoEmployee Employee)
+        {
+            context.Add(Employee);
+            context.SaveChanges();
+        }
     }
 }
