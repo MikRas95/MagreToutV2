edbc5be1b5f2c108d1f0d5ff1445da5cdf046aaf
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2021-12-07 12:28:43 +0100

    AddAd funktion med html

--- MagreToutV2/Pages/Ads/Create.cshtml.cs
+++ MagreToutV2/Pages/Ads/Create.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MalgreToutV2.Models;
+using MalgreToutV2.Services.Interface;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,29 @@ namespace MalgreToutV2.Pages.Ads
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public DemoAd Ad { get; set; }
+        public IAd service;
+
+
+        public CreateModel(IAd Service)
         {
+            service = Service;
+            Ad = new DemoAd();
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            service.AddAd(Ad);
+
+            return RedirectToPage("/Ads/Read");
         }
     }
 }
