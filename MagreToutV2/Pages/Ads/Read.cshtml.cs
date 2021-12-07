5388c995ffd5aa0af90d5648ea0b1be6a0961e7f
Jens Peter Thorsen <jens6829@edu.easj.dk>
2021-12-07 11:30:11 +0000

    REadad

--- MagreToutV2/Pages/Ads/Read.cshtml.cs
+++ MagreToutV2/Pages/Ads/Read.cshtml.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MalgreToutV2.Models;
+using MalgreToutV2.Services.EFServices;
+using MalgreToutV2.Services.Interface;
 
 namespace MalgreToutV2.Pages.Ads
 {
     public class ReadModel : PageModel
     {
+        [BindProperty]
+        public IEnumerable<DemoAd> ads { get; set; }
+        private IAd AdService;
+        public ReadModel(IAd service)
+        {
+            AdService = service;
+            ads = new List<DemoAd>();
+        }
         public void OnGet()
         {
+            ads = AdService.GetAds();
         }
     }
 }
